2ad00bf74f4875eb396fb5ee02b2399c4e759737
Sebastian Volk <volk.sebastian@outlook.de>
2020-03-01 15:14:09 +0100

    Add configuration

---
+++ src/Resources/PluginConfig.php
@@ -0,0 +1,42 @@
+<?php declare(strict_types=1);
+
+namespace Swag\PluginConfig;
+
+use Shopware\Core\Framework\Context;
+use Shopware\Core\Framework\DataAbstractionLayer\EntityRepositoryInterface;
+use Shopware\Core\Framework\DataAbstractionLayer\Search\Criteria;
+use Shopware\Core\Framework\DataAbstractionLayer\Search\Filter\ContainsFilter;
+use Shopware\Core\Framework\Plugin;
+use Shopware\Core\Framework\Plugin\Context\UninstallContext;
+
+class PluginConfig extends Plugin
+{
+    public function uninstall(UninstallContext $uninstallContext): void
+    {
+        parent::uninstall($uninstallContext);
+
+        if ($uninstallContext->keepUserData()) {
+            return;
+        }
+
+        $this->removeConfiguration($uninstallContext->getContext());
+    }
+
+    private function removeConfiguration(Context $context): void
+    {
+        /** @var EntityRepositoryInterface $systemConfigRepository */
+        $systemConfigRepository = $this->container->get('system_config.repository');
+        $criteria = (new Criteria())->addFilter(new ContainsFilter('configurationKey', $this->getName() . '.config.'));
+        $idSearchResult = $systemConfigRepository->searchIds($criteria, $context);
+
+        $ids = array_map(static function ($id) {
+            return ['id' => $id];
+        }, $idSearchResult->getIds());
+
+        if ($ids === []) {
+            return;
+        }
+
+        $systemConfigRepository->delete($ids, $context);
+    }
+}
